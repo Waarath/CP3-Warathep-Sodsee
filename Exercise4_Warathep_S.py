c264c5590e165d6ca405429957c62c087f50fbb2
Waarath <savescter@icloud.com>
2022-07-09 10:48:59 +0700

    Exercise4_Variable_Score
    Add variable / Your score(Text) / Score(integer,float)

---
+++ Exercise4_Warathep_S.py
@@ -0,0 +1,9 @@
+fe = "Foundation English"
+gb = "General Business"
+ics = "Introduction to Computer Systems"
+cp = "Computer Programming"
+print("---Your Score---")
+print(fe,"     :",74.2)
+print(gb,"       :",68.6)
+print(ics,":",72)
+print(cp,"   :",70.4)
