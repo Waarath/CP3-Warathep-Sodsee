a8a5b5fdeddc9e3452431e5422558bc40da0e95b
Waarath <savescter@icloud.com>
2022-08-01 13:33:13 +0700

    Create Lecture 50_Warathep_S.py

---
+++ Lecture 50_Warathep_S.py
@@ -0,0 +1,18 @@
+def AddNumber(x, y):
+    print(x + y)
+AddNumber(10, 2)
+
+
+def SubtractNumber(x, y):
+    print(x - y)
+SubtractNumber(10, 5)
+
+
+def MultiplyNumber(x, y):
+    print(x * y)
+MultiplyNumber(10, 5)
+
+
+def DivideNumber(x, y):
+    print(x / y)
+DivideNumber(10, 5)
